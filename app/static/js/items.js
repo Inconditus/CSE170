a6183b8799637b5b944864a6b6b0aae803ef16af
Justin Hung <juhung@ucsd.edu>
2014-02-27 01:40:56 -0800

    clean up a bit

--- app/static/js/items.js
+++ app/static/js/items.js
@@ -1,81 +1,13 @@
-$(document).ready( function() {
-			var JSONinfo = $.cookie("newitem") ;
-			var JSONobj = JSON.parse(JSONinfo);
+// //doesnt work, only display info of the last item
+// $(".item").click( function() {
+// 	var id = document.getElementById("url").attributes["href"].value;
+// 	window.location.href = url
+// });
 
-			$(".content").prepend("<div class='item'><div class='name'>" + JSONobj.name + "</div><div class='desc'>" + JSONobj.desc + "</div><img class='img-responsive' src=" + JSONobj.imgsrc + ">" + "<div class='itembar'><div class='btn-itembar price'>$" + JSONobj.price + "</div><div class='btn-itembar origprice'>$" + JSONobj.origprice + "</div><div class='btn-itembar btn-buy'>More Details</div></div></div><br>");
+$(".add").click( function() {
+	window.location.href = "/add/";
+});
 
-			$(".item").click( function() {
-
-				console.log($(this).find(".name").html());
-
-				var imgsrc = $(this).find(".img-responsive").attr("src");
-				var name = $(this).find(".name").html();
-				var desc = $(this).find(".desc").html();
-				var origprice = $(this).find(".origprice").html();
-				var price = $(this).find(".price").html();
-				var seller = $(this).find(".seller").html();
-
-				var JSONinfo = {}
-
-				JSONinfo["name"] = name;
-				JSONinfo["desc"] = desc;
-				JSONinfo["origprice"] = origprice.substr(1);
-				JSONinfo["price"] = price.substr(1);
-				JSONinfo["imgsrc"] = imgsrc;
-				JSONinfo["seller"] = seller;
-
-				// 1 min expiry for cookie
-				// var date = new Date();
-				// date.setTime(date.getTime() + (1 * 60 * 1000));
-
-				$.cookie("listinginfo", JSON.stringify(JSONinfo) );
-
-				console.log(JSON.stringify(JSONinfo));
-
-				window.location.href = "/singleitem";
-				
-			});
-
-			
-		});
-	
-	// After clicking item, send information to a cookie so that the singleitem page can render the cookie
-	$(".item").click( function() {
-
-		console.log($(this).find(".name").html());
-
-		var imgsrc = $(this).find(".img-responsive").attr("src");
-		var name = $(this).find(".name").html();
-		var desc = $(this).find(".desc").html();
-		var origprice = $(this).find(".origprice").html();
-		var price = $(this).find(".price").html();
-		var seller = $(this).find(".seller").html();
-
-		var JSONinfo = {}
-
-		JSONinfo["name"] = name;
-		JSONinfo["desc"] = desc;
-		JSONinfo["origprice"] = origprice.substr(1);
-		JSONinfo["price"] = price.substr(1);
-		JSONinfo["imgsrc"] = imgsrc;
-		JSONinfo["seller"] = seller;
-
-		// 1 min expiry for cookie
-		// var date = new Date();
-		// date.setTime(date.getTime() + (1 * 60 * 1000));
-
-		$.cookie("listinginfo", JSON.stringify(JSONinfo) );
-
-		console.log(JSON.stringify(JSONinfo));
-
-		window.location.href = "/singleitem";
-		
-	});
-
-	$(".add").click( function() {
-		window.location.href = "/add/";
-	});
-
-	$(".manage").click( function() {
-		window.location.href = "/manage/";
-	});
+$(".manage").click( function() {
+	window.location.href = "/manage/";
+});
--- app/static/js/singleitem.js
+++ app/static/js/singleitem.js
@@ -1,93 +1,95 @@
-$(document).ready( function() {
-			var JSONinfo = $.cookie("listinginfo") ;
-			var JSONobj = JSON.parse(JSONinfo);
+//whole file isnt needed 
 
-			$("#imgsrc").attr("src", JSONobj.imgsrc);
-			$("#name").html(JSONobj.name);
-			$("#desc").html(JSONobj.desc);
-			$("#price").html(JSONobj.price);
-			$("#origprice").html(JSONobj.origprice);
-			$("#seller").html(JSONobj.seller);
+// $(document).ready( function() {
+// 			var JSONinfo = $.cookie("listinginfo") ;
+// 			var JSONobj = JSON.parse(JSONinfo);
 
-			// if the box for a min amount of buyers was checked
-			if (JSONobj.check === true) 
-			{
-				// Insert the min amount of buyers
-				// TODO: This is temporary as we should show the current 
-				// buyers/total buyers needed but the current buyers should come 
-				// from the backend
-				$(".input-group p:last-child").after("<p><span class='labels'>Minimum Buyers:</span><span id='minnum'>20</span></p>");
-				$("#minnum").html(JSONobj.minnum);
+// 			$("#imgsrc").attr("src", JSONobj.imgsrc);
+// 			$("#name").html(JSONobj.name);
+// 			$("#desc").html(JSONobj.desc);
+// 			$("#price").html(JSONobj.price);
+// 			$("#origprice").html(JSONobj.origprice);
+// 			$("#seller").html(JSONobj.seller);
 
-			}
+// 			// if the box for a min amount of buyers was checked
+// 			if (JSONobj.check === true) 
+// 			{
+// 				// Insert the min amount of buyers
+// 				// TODO: This is temporary as we should show the current 
+// 				// buyers/total buyers needed but the current buyers should come 
+// 				// from the backend
+// 				$(".input-group p:last-child").after("<p><span class='labels'>Minimum Buyers:</span><span id='minnum'>20</span></p>");
+// 				$("#minnum").html(JSONobj.minnum);
 
-			$(".myButton").click( function() {
-				alert("Awesome! You have opted into buying " + JSONobj.name +"! You will be notified when " + JSONobj.seller + " has purchased the item and is ready to be picked up.");
+// 			}
 
-				var JSONinfo = $.cookie("manage");
-				// If have not bought any item yet
-				console.log(JSONinfo);
-				if(typeof JSONinfo === 'undefined'){
-					var manage = {};
-					manage["count"] = 1;
+// 			$(".myButton").click( function() {
+// 				alert("Awesome! You have opted into buying " + JSONobj.name +"! You will be notified when " + JSONobj.seller + " has purchased the item and is ready to be picked up.");
 
-					var item = {};
-					//item["check"] = check;
-					item["name"] = JSONobj.name;
-					item["desc"] = JSONobj.desc;
-					//item["origprice"] = origprice;
-					item["price"] = JSONobj.price;
-					item["imgsrc"] = JSONobj.imgsrc;
-					//item["minnum"] = minnum;
-					item["seller"] = JSONobj.seller;
+// 				var JSONinfo = $.cookie("manage");
+// 				// If have not bought any item yet
+// 				console.log(JSONinfo);
+// 				if(typeof JSONinfo === 'undefined'){
+// 					var manage = {};
+// 					manage["count"] = 1;
 
-					manage["item1"] = item;
+// 					var item = {};
+// 					//item["check"] = check;
+// 					item["name"] = JSONobj.name;
+// 					item["desc"] = JSONobj.desc;
+// 					//item["origprice"] = origprice;
+// 					item["price"] = JSONobj.price;
+// 					item["imgsrc"] = JSONobj.imgsrc;
+// 					//item["minnum"] = minnum;
+// 					item["seller"] = JSONobj.seller;
 
-					console.log(manage);
+// 					manage["item1"] = item;
 
-					var date = new Date();
-						date.setTime(date.getTime() + (5 * 60 * 1000));
-					$.cookie("manage", JSON.stringify(manage), {expires: date});
-				}
-				else
-				{
-					var obj = JSON.parse(JSONinfo);
-					obj["count"] = obj.count + 1;
-					console.log(obj);
+// 					console.log(manage);
 
-					var item = {};
-					//item["check"] = check;
-					item["name"] = JSONobj.name;
-					item["desc"] = JSONobj.desc;
-					//item["origprice"] = origprice;
-					item["price"] = JSONobj.price;
-					item["imgsrc"] = JSONobj.imgsrc;
-					//item["minnum"] = minnum;
-					item["seller"] = JSONobj.seller;
+// 					var date = new Date();
+// 						date.setTime(date.getTime() + (5 * 60 * 1000));
+// 					$.cookie("manage", JSON.stringify(manage), {expires: date});
+// 				}
+// 				else
+// 				{
+// 					var obj = JSON.parse(JSONinfo);
+// 					obj["count"] = obj.count + 1;
+// 					console.log(obj);
 
-					var newitem = "item" + obj.count;
+// 					var item = {};
+// 					//item["check"] = check;
+// 					item["name"] = JSONobj.name;
+// 					item["desc"] = JSONobj.desc;
+// 					//item["origprice"] = origprice;
+// 					item["price"] = JSONobj.price;
+// 					item["imgsrc"] = JSONobj.imgsrc;
+// 					//item["minnum"] = minnum;
+// 					item["seller"] = JSONobj.seller;
 
-					obj[newitem] = item;
+// 					var newitem = "item" + obj.count;
 
-					var date = new Date();
-						date.setTime(date.getTime() + (5 * 60 * 1000));
-					$.cookie("manage", JSON.stringify(obj), {expires: date});
-				}
+// 					obj[newitem] = item;
 
-				//window.location.href = "http://107.170.250.75/CSE170/frontend/items.html";
+// 					var date = new Date();
+// 						date.setTime(date.getTime() + (5 * 60 * 1000));
+// 					$.cookie("manage", JSON.stringify(obj), {expires: date});
+// 				}
 
-			});
+// 				//window.location.href = "http://107.170.250.75/CSE170/frontend/items.html";
 
-			$("#seller").click( function() {
-				var profile = {}
+// 			});
 
-				profile["user"] = JSONobj.seller;
+// 			$("#seller").click( function() {
+// 				var profile = {}
 
-				var date = new Date();
-				date.setTime(date.getTime() + (5 * 60 * 1000));
+// 				profile["user"] = JSONobj.seller;
 
-				$.cookie("user", JSON.stringify(profile), { expires: date } );
+// 				var date = new Date();
+// 				date.setTime(date.getTime() + (5 * 60 * 1000));
 
-				window.location.href = "http://107.170.250.75/CSE170/frontend/profile.html";
-			});
-		});
+// 				$.cookie("user", JSON.stringify(profile), { expires: date } );
+
+// 				window.location.href = "http://107.170.250.75/CSE170/frontend/profile.html";
+// 			});
+// 		});
