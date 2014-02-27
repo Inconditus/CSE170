5f0b24cdd2ddf5a9f8fc67245fd9504655b6353d
Theresa Lee <t10lee@ucsd.edu>
2014-02-27 13:18:57 -0800

    Search function

--- app/static/js/items.js
+++ app/static/js/items.js
@@ -3,28 +3,29 @@
 // 	var id = document.getElementById("url").attributes["href"].value;
 // 	window.location.href = url
 // });
+$(document).ready(function(){
+	$(".add").click( function() {
+		window.location.href = "/add/";
+	});
 
-$(".add").click( function() {
-	window.location.href = "/add/";
-});
+	$(".manage").click( function() {
+		window.location.href = "/manage/";
+	});
 
-$(".manage").click( function() {
-	window.location.href = "/manage/";
-});
+	$("#search").keyup(function(){
+	    var filter = $(this).val();
 
-$("#search").keyup(function(){
-    var filter = $(this).val();
+	    // This is the word to search for
+	    var searchterm = $("#search").val().toLowerCase();
+	    $(".item").each(function(){
 
-    // This is the word to search for
-    var searchterm = $("#search").val().toLowerCase();
-    $(".item").each(function(){
-
-       // If not contain, fade out
-       var item = $(this).children(".name").html().toLowerCase();
-		if( item.indexOf(searchterm) < 0 )
-			$(this).fadeOut();
-        else {
-            $(this).show();
-        }
-    });
+	       // If not contain, fade out
+	       var item = $(this).children(".name").html().toLowerCase();
+			if( item.indexOf(searchterm) < 0 )
+				$(this).fadeOut();
+	        else {
+	            $(this).show();
+	        }
+	    });
+	});
 });
